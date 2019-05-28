b26bf617768732ee195c21cf1ac445b676f48564
Szymon Wrzesień <szymon.wrzesien09@gmail.com>
2019-05-28 13:00:28 +0200

    commit

---
+++ DekoratorNJPO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dekorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
